46567c9e60f79dd673198814be11a984039f8745
chealserg <chealserg@gmail.com>
2020-05-18 00:08:11 +0200

    initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+  <b-navbar type="light" variant="white">
+    <b-navbar-nav class="w-100" fill justified>
+      <b-nav-item active href="#">Information</b-nav-item>
+      <b-nav-item href="#">Foto & Video</b-nav-item>
+      <b-nav-item href="#"><img src="../assets/logo-1.jpg"></b-nav-item>
+      <b-nav-item href="#">Map</b-nav-item>
+      <b-nav-item href="#">Hotels</b-nav-item>
+    </b-navbar-nav>
+  </b-navbar>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    li {
+        font-weight: bold;
+    }
+</style>
+
---
+++ src/views/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <lol desc="lol, kek, cheburek" />
+  </div>
+</template>
+
+<script>
+import Lol from '@/components/Lol.vue'
+export default {
+  name: 'About',
+  components: {
+    Lol
+  }
+}
+</script>
