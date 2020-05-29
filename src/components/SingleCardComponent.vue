480af81faf04f9475f6bb9c499a993c3cd9740a1
chealserg <chealserg@gmail.com>
2020-05-29 20:54:10 +0200

    MainPage-Sedona-vol.1

---
+++ src/components/SingleCardComponent.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+     <b-row>
+       <b-col cols="12" class="bgr-light-blue">
+         <div class="d-flex single-car-section">
+            <Textcard />
+             <img class="single-card-section-image" :src="imgUrl">  
+          </div>
+      </b-col>
+    </b-row>
+ </div>
+</template>
+
+<script>
+
+import Textcard from '@/components/Textcard.vue'
+
+export default {
+  name: 'SingleCardSection',
+  props: [
+      "cardHeader",
+      "subHeader",
+      "text",
+      "direction",
+      "imgUrl"
+  ],
+  components: {
+    Textcard
+  }
+}
+</script>
+
+<style scoped>
+
+.single-card-section-image {
+    width: calc(100% - 400px);
+}
+.single-car-section {
+    height: 260px;
+}
+  
+</style>
